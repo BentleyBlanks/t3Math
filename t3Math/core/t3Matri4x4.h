a139fe22fdebf4bf0ecafb83228eed301f1d3dac
BentleyBlanks <bentleyjobs@gmail.com>
2016-02-26 02:15:50 +0800

    更新t3Vector4的支持，提供未实现的t3Math API

--- t3Math/core/t3Matri4x4.h
+++ t3Math/core/t3Matri4x4.h
@@ -1,19 +1,19 @@
 ﻿#ifndef T3_MATRIX4_H
 #define T3_MATRIX4_H
 
-class t3Matrix4
+class t3Matri4x4
 {
 public:
-	t3Matrix4();
+	t3Matri4x4();
 
-	t3Matrix4(float a00, float a01, float a02, float a03,
+    t3Matri4x4(float matrix[4][4]);
+
+	t3Matri4x4(float a00, float a01, float a02, float a03,
 		float a10, float a11, float a12, float a13,
 		float a20, float a21, float a22, float a23,
 		float a30, float a31, float a32, float a33);
 
-	~t3Matrix4()
-	{
-	}
+    ~t3Matri4x4();
 
 	void set(float a00, float a01, float a02, float a03,
 		float a10, float a11, float a12, float a13,
@@ -26,8 +26,7 @@ public:
 
 	void makeOrtho2DMatrix(double left, double right, double bottom, double top);
 
-	float _mat[4][4];
-
+	float m[4][4];
 };
 
 #endif
